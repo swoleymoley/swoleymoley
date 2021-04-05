608aa7863f91fc7991145c3ac2184eb9ec5e98b8
Jeff <jeff.halley@gmail.com>
2021-04-04 19:59:51 -0700

    Starting on Core Data, using Dorkout as code for CoreData based workouts

--- FrontEndTest/FrontEndTest/FrontEndTestApp.swift
+++ FrontEndTest/FrontEndTest/FrontEndTestApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct FrontEndTestApp: App {
+    let persistentContainer: NSPersistentContainer = {
+          let container = NSPersistentContainer(name: "SwoleyMoleyDataModel")
+          container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+              if let error = error as NSError? {
+                  fatalError("Unresolved error \(error), \(error.userInfo)")
+              }
+          })
+          return container
+      }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
